4a554fbbdb9e88af4b274adb244b8ffa3f4e50ac
Tom <tom@matterfi.com>
2022-04-26 20:52:03 -0500

    Set thread names to aid with debugging.

--- src/blockchain/node/wallet/subchain/statemachine/Process.cpp
+++ src/blockchain/node/wallet/subchain/statemachine/Process.cpp
@@ -53,6 +53,7 @@
 #include "opentxs/util/Log.hpp"
 #include "opentxs/util/Pimpl.hpp"
 #include "util/ScopeGuard.hpp"
+#include "util/Thread.hpp"
 #include "util/Work.hpp"
 
 namespace opentxs::blockchain::node::wallet
@@ -422,7 +423,8 @@ auto Process::Imp::queue_process() noexcept -> bool
              post = std::make_shared<ScopeGuard>(
                  [this] { ++running_; }, [this] { --running_; }),
              pos{i->first},
-             ptr{i->second}] { do_process(pos, ptr); });
+             ptr{i->second}] { do_process(pos, ptr); },
+            "Process block");
     }
 
     return have_items();
--- src/util/Actor.hpp
+++ src/util/Actor.hpp
@@ -193,6 +193,7 @@ protected:
         , reorg_lock_()
         , pipeline_(api.Network().ZeroMQ().Internal().Pipeline(
               {},
+              name,
               subscribe,
               pull,
               dealer,
